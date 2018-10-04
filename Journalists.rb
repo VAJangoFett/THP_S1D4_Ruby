8bf4ea88ab61f4600a9a9294f75050c8ff304b94
Thierry <kasskq@gmail.com>
2018-10-04 17:41:30 +0200

    version finale Journalistes

--- Journalists.rb
+++ Journalists.rb
@@ -1,31 +1,37 @@
 ary = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
-#1
+#1 compte le nombre de journaliste
 def length(value)
-	puts "There are #{value.length} journalists."
+	puts "Il y a #{value.length} journalistes"
+	perform(value)
 end
 
-#2
-def integer(value)
-		return value.count("0-9") > 0
+#2 trouve le nombre d emails avec un chiffre
+def integer(array)
+ 	count = 0
+ 	array.each do |value|
+ 		if value.count("0-9") > 0
+ 			count += 1
+ 		end
+ 	end
+ 	puts "Il y a une #{count} emails avec un chiffre"
+ 	perform(array)
 end
 
-result = ary.select{|journalist| integer(journalist)}
-
-# puts result.length
-
-#3
-
-def aude_met(value)
-	aude_name = value =~ /(A|a)(U|u)(D|d)(E|e)/
-	return aude_name
-
+#3 trouve toutes les audes dans les adresses mail
+def aude_met(array)
+    count = 0
+    array.each do |value|
+    aude_name = value =~ /(A|a)(U|u)(D|d)(E|e)/
+        if aude_name != nil
+            count += 1
+        end
+    end
+    puts "Il y a #{count} Aude dans les mails"
+    perform(array)
 end
 
-check = ary.select{|aude| aude_met(aude)}
-puts "#{check.length} aude"
-
-#4
+#4 Trouve touts les mails commencant par @ majuscule
 def capitalize(array)
 	count =0
 	array.each do |value|
@@ -33,12 +39,11 @@ def capitalize(array)
 			count +=1
 		end
 	end
-	return count
+	puts "Il y a #{count} mails commencant par une majuscule"
+	perform(array)
 end
 
-#puts capitalize(ary)
-
-#5
+#5 Trouve touts les mails avec une majuscule
 def capitalize_in(array)
 	count =0
 	array.each do |value|
@@ -46,29 +51,74 @@ def capitalize_in(array)
 			count +=1
 		end
 	end
-	return count
+	puts "Il y a #{count} mails avec une majuscule"
+	perform(array)
 end
 
-#puts capitalize_in(ary)
 
-#6
+#6 Compte le nombre de underscore
 def underscore(array)
 	count = 0
 	array.each do |value|
 		count += value.count('_')
 		end
-	return count
+	puts "Il y a #{count} underscore dans la liste de mails"
+	perform(array)
 end
 
-#puts underscore(ary)
 
-#7
+#7 Trie par ordre alphabetique
 def alphabetical(array)
 	puts array.sort_by{|order| order.downcase}
+	perform(array)
 end
 
-#alphabetical(ary)
+def perform(ary)
+	puts ""
+	puts ""
+	puts ""
+	puts "<<<<<<<<<<Bienvenue, quel programme voulez vous lancer?>>>>>>>>>>"
+	puts "1. Combien y a t'il de journalistes?"
+	puts "2. Combien d'emails contiennent un chiffre?"
+	puts "3. Combien d'email contiennent le prenom Aude?"
+	puts "4. Combien d'emails commencent par une majuscule?"
+	puts "5. Combien contiennent une majuscule?"
+	puts "6. Combien de underscore dans les emails?"
+	puts "7. Liste triee par ordre alphabetique."
+	puts "8. Quitter le programme"
+	puts ""
+	puts "Votre choix :"
+	puts ""
+	puts ""
+	print ">>"
+	choice = gets.chomp.to_i
+	puts ""
+	puts ""
+	puts ""
+	if choice == 1
+		length(ary)
+	elsif choice == 2
+		integer(ary)
+	elsif choice == 3
+		aude_met(ary)
+	elsif choice == 4
+		capitalize(ary)
+	elsif choice == 5
+		capitalize_in(ary)
+	elsif choice == 6
+		underscore(ary)
+	elsif choice == 7
+		alphabetical(ary)
+	elsif choice == 8
+		puts "Au revoir !!"
+		return 0
+	else
+		puts "Mauvaise entree"
+		perform
+	end
+end
 
+perform(ary)
 
 
 
